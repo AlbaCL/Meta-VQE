cca4f0c8510fa5556fb0dfeaaa6a1906a689e698
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-09-29 21:36:08 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+# meta-VQE does not need to be installed, but this will take care of the tequila installation
+import setuptools
+
+setuptools.setup(
+    install_requires= ['tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila' ]
+)
+
